0ae9531c53baffaedf6ccfc4987803a2a0bd6ef9
sdias-code <silviodias.ms@gmail.com>
2021-09-01 15:23:53 -0400

    Add Interface e Repository

---
+++ ApiByteBank/Repositories/GenericRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiByteBank.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiByteBank.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private SilvioBbAppBancoContext context = null;
+        public GenericRepository(SilvioBbAppBancoContext _context)
+        {
+            this.context = _context;
+        }
+
+        public async Task<IEnumerable<T>> GetAll()
+        {
+            return await context.Set<T>().AsNoTracking().ToListAsync();
+        }
+        public async Task<T> GetById(int id)
+        {
+            return await context.Set<T>().FindAsync(id);
+        }
+        public async Task Insert(T obj)
+        {
+            await context.Set<T>().AddAsync(obj);
+            await context.SaveChangesAsync();
+        }
+        public async Task Update(int id, T obj)
+        {
+            context.Set<T>().Update(obj);
+            await context.SaveChangesAsync();
+        }
+        public async Task Delete(int id)
+        {
+            var entity = await GetById(id);
+            context.Set<T>().Remove(entity);
+            await context.SaveChangesAsync();
+        }
+    }
+}
