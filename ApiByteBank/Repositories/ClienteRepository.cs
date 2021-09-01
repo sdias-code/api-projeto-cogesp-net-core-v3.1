984e9d26079362c3e69b694067b7dba0a64c15b9
sdias-code <silviodias.ms@gmail.com>
2021-09-01 15:44:58 -0400

    Add interface e repository cliente

---
+++ ApiByteBank/Repositories/ClienteRepository.cs
@@ -0,0 +1,16 @@
+﻿using ApiByteBank.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiByteBank.Repositories
+{
+    public class ClienteRepository : GenericRepository<Cliente>, IClienteRepository
+    {
+        public ClienteRepository(SilvioBbAppBancoContext repositoryContext) : base(repositoryContext)
+        {
+
+        }
+    }
+}
--- ApiByteBank/Startup.cs
+++ ApiByteBank/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ApiByteBank.Models;
+using ApiByteBank.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -35,6 +36,8 @@ namespace ApiByteBank
 
             );
 
+            
+            services.AddScoped<IClienteRepository, ClienteRepository>();
             services.AddControllers();
         }
 
