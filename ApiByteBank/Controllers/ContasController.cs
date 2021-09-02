5f2e5b51999638219bb71ed191552e5d6d0992b6
sdias-code <silviodias.ms@gmail.com>
2021-09-02 09:31:45 -0400

    Criado controllers cliente, conta e transacao

---
+++ ApiByteBank/Controllers/ContasController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ApiByteBank.Models;
+
+namespace ApiByteBank.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContasController : ControllerBase
+    {
+        private readonly SilvioBbAppBancoContext _context;
+
+        public ContasController(SilvioBbAppBancoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Contas
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Conta>>> GetContas()
+        {
+            return await _context.Contas.ToListAsync();
+        }
+
+        // GET: api/Contas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Conta>> GetConta(int id)
+        {
+            var conta = await _context.Contas.FindAsync(id);
+
+            if (conta == null)
+            {
+                return NotFound();
+            }
+
+            return conta;
+        }
+
+        // PUT: api/Contas/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutConta(int id, Conta conta)
+        {
+            if (id != conta.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(conta).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ContaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Contas
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Conta>> PostConta(Conta conta)
+        {
+            _context.Contas.Add(conta);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetConta", new { id = conta.Id }, conta);
+        }
+
+        // DELETE: api/Contas/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Conta>> DeleteConta(int id)
+        {
+            var conta = await _context.Contas.FindAsync(id);
+            if (conta == null)
+            {
+                return NotFound();
+            }
+
+            _context.Contas.Remove(conta);
+            await _context.SaveChangesAsync();
+
+            return conta;
+        }
+
+        private bool ContaExists(int id)
+        {
+            return _context.Contas.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ ApiByteBank/Controllers/TransacoesController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ApiByteBank.Models;
+
+namespace ApiByteBank.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransacoesController : ControllerBase
+    {
+        private readonly SilvioBbAppBancoContext _context;
+
+        public TransacoesController(SilvioBbAppBancoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Transacoes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Transaco>>> GetTransacoes()
+        {
+            return await _context.Transacoes.ToListAsync();
+        }
+
+        // GET: api/Transacoes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Transaco>> GetTransaco(int id)
+        {
+            var transaco = await _context.Transacoes.FindAsync(id);
+
+            if (transaco == null)
+            {
+                return NotFound();
+            }
+
+            return transaco;
+        }
+
+        // PUT: api/Transacoes/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTransaco(int id, Transaco transaco)
+        {
+            if (id != transaco.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(transaco).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TransacoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Transacoes
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Transaco>> PostTransaco(Transaco transaco)
+        {
+            _context.Transacoes.Add(transaco);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTransaco", new { id = transaco.Id }, transaco);
+        }
+
+        // DELETE: api/Transacoes/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Transaco>> DeleteTransaco(int id)
+        {
+            var transaco = await _context.Transacoes.FindAsync(id);
+            if (transaco == null)
+            {
+                return NotFound();
+            }
+
+            _context.Transacoes.Remove(transaco);
+            await _context.SaveChangesAsync();
+
+            return transaco;
+        }
+
+        private bool TransacoExists(int id)
+        {
+            return _context.Transacoes.Any(e => e.Id == id);
+        }
+    }
+}
