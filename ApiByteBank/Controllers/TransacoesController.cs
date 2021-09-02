246f602d1cf3e01bd28dc7dc0b1abfeefbe78264
sdias-code <silviodias.ms@gmail.com>
2021-09-02 13:25:50 -0400

    Add metodo deposito

--- ApiByteBank/Controllers/TransacoesController.cs
+++ ApiByteBank/Controllers/TransacoesController.cs
@@ -44,14 +44,64 @@ namespace ApiByteBank.Controllers
         // POST: api/Transacoes
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        //[HttpPost]
+        //public async Task<ActionResult<Transaco>> PostTransaco(Transaco transaco)
+        //{
+        //    _context.Transacoes.Add(transaco);
+        //    await _context.SaveChangesAsync();
+
+        //    return CreatedAtAction("GetTransaco", new { id = transaco.Id }, transaco);
+        //}
+
+        //Deposito: api/Transacoes
         [HttpPost]
-        public async Task<ActionResult<Transaco>> PostTransaco(Transaco transaco)
+        public async Task<ActionResult<Transaco>> DepositoTransaco(Transaco transaco)
+
         {
-            _context.Transacoes.Add(transaco);
-            await _context.SaveChangesAsync();
+            Conta contaDB = _context.Contas.Find(transaco.ContaId);
+            var tipo = transaco.TipoTransacaoId;
+            
+            double valor = transaco.Valor;
+            
+
+            if(valor <= 0)
+            {
+                return BadRequest("Não é permitido entrar com valor 0 ou negativo!");
+            }
+
+            if(tipo == 1)
+            {
+                double saldo_Atual = contaDB.Saldo;                
+
+                double deposito = saldo_Atual + valor;
+
+                contaDB.Saldo = deposito;
+
+                transaco.Valor = valor;
+                transaco.Data = DateTime.Now;
+
+                _context.Transacoes.Add(transaco);
+
+                await _context.SaveChangesAsync();
+
+               
+            }
 
             return CreatedAtAction("GetTransaco", new { id = transaco.Id }, transaco);
+
         }
+
+        //Retirada: api/Transacoes
+        //[HttpPost]
+        //public async Task<ActionResult<Transaco>> SaqueTransaco(Transaco transaco)
+
+        //{
+
+        //    _context.Transacoes.Add(transaco);
+        //    await _context.SaveChangesAsync();
+
+        //    return CreatedAtAction("GetTransaco", new { id = transaco.Id }, transaco);
+        //}
 
         // DELETE: api/Transacoes/5
         [HttpDelete("{id}")]
